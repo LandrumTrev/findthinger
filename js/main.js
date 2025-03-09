5638078963aac9bc263dd1dbb63c66c2e946f8e1
Rich Trevillian <30249360+LandrumTrev@users.noreply.github.com>
2025-03-09 03:12:59 -0600

    Second Commit - Testing 123

--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@ document.addEventListener('DOMContentLoaded', () => {
    app.innerHTML = '<h1>Welcome to Findthinger</h1>';
    console.log('Findthinger initialized');
 });
+
+// This is absolutely the smallest change I could possibly think of making to this file, just to make sure the local to GitHub connection is working. Really.
