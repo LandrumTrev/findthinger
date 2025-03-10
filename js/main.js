25ac39e60b241c1c2be0fa724848dec878f06c8c
Rich Trevillian <30249360+LandrumTrev@users.noreply.github.com>
2025-03-10 00:53:07 -0600

    Testing local to GitHub to SiteGround

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
    const app = document.getElementById('app');
-   app.innerHTML = '<h1>Welcome to Findthinger</h1>';
+   app.innerHTML = '<h1>Well hello world! Bienvenudo to Findthinger!</h1>';
    console.log('Findthinger initialized');
 });
 
