589562818eff544f9025a6682ceb731fdb76b0a9
Rich Trevillian <30249360+LandrumTrev@users.noreply.github.com>
2025-03-11 00:25:52 -0600

    ESLint linting and Prettier formatting added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-   const app = document.getElementById('app');
-   app.innerHTML = '<h1>Well howdy world! Bienvenudo to Findthinger!</h1>';
-   console.log('Findthinger initialized');
+document.addEventListener("DOMContentLoaded", () => {
+  const app = document.getElementById("app");
+  app.innerHTML = "<h1>Well howdy world! Bienvenudo to Findthinger!</h1>";
+  console.log("Findthinger initialized");
 });
 
 // This is absolutely the smallest change I could possibly think of making to this file, just to make sure the local to GitHub connection is working. Really.
