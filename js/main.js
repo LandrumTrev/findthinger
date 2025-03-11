e6fb45818cf661495bb39bc2ba742ed27db7ab2f
Rich Trevillian <30249360+LandrumTrev@users.noreply.github.com>
2025-03-11 02:52:01 -0600

    installed Sass compilier

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
-  app.innerHTML = "<h1>Well howdy world! Bienvenudo to Findthinger!</h1>";
+  app.innerHTML = "<h1>Well howdy, Earth! Bienvenudo to Findthinger! <br>And a big shout out to our friends on Mars as well. <br><small>A separate but equally warm greetings to those watching from the Moon!</small></h1>";
   console.log("Findthinger initialized");
 });
 
