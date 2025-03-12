48a50ed5168bc3a97c2538e07d7f24e5fdff4894
Rich Trevillian <30249360+LandrumTrev@users.noreply.github.com>
2025-03-11 23:26:47 -0600

    hosting cleanout, reupload all

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
-  app.innerHTML = "<h1>The Findthinger</h1>";
+  app.innerHTML = "<h1>Findthinger, the thing to find things with.</h1>";
   console.log("Findthinger initialized");
 });
