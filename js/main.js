bf4a3d86f9496dae2a5e055fbbfa184699d2b8e7
Rich Trevillian <30249360+LandrumTrev@users.noreply.github.com>
2025-03-11 23:15:47 -0600

    General monkeybusiness

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
-  app.innerHTML = "<h1>Knick Knack.<br><small>Paddywack.</small></h1>";
+  app.innerHTML = "<h1>The Findthinger</h1>";
   console.log("Findthinger initialized");
 });
-
-// This is absolutely the smallest change I could possibly think of making to this file, just to make sure the local to GitHub connection is working. Really.
