c532754f646204655ac0bbab46303e728d821612
Rich Trevillian <30249360+LandrumTrev@users.noreply.github.com>
2025-03-11 23:46:35 -0600

    changes check

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
-  app.innerHTML = "<h1>Findthinger, the thing to find things with.</h1>";
+  app.innerHTML = "<h1>Findthinger, the thing to find things with.<br><small>Now with 30% more moxie!</small></h1>";
   console.log("Findthinger initialized");
 });
